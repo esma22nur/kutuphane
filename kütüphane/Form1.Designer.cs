8b61fde3008ddb3da2e1842d422a030578caba7d
esma22nur <esmanursoysal@hotmail.com>
2024-05-13 16:24:56 +0300

    Add project files.

---
+++ kütüphane/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace kütüphane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnkaydet = new System.Windows.Forms.Button();
+            this.btnlistele = new System.Windows.Forms.Button();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btnara = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtbxkitapara = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kitap ara:";
+            // 
+            // btnkaydet
+            // 
+            this.btnkaydet.Location = new System.Drawing.Point(375, 89);
+            this.btnkaydet.Name = "btnkaydet";
+            this.btnkaydet.Size = new System.Drawing.Size(103, 42);
+            this.btnkaydet.TabIndex = 1;
+            this.btnkaydet.Text = "Kaydet";
+            this.btnkaydet.UseVisualStyleBackColor = true;
+            this.btnkaydet.Click += new System.EventHandler(this.btnkaydet_Click);
+            // 
+            // btnlistele
+            // 
+            this.btnlistele.Location = new System.Drawing.Point(375, 146);
+            this.btnlistele.Name = "btnlistele";
+            this.btnlistele.Size = new System.Drawing.Size(103, 42);
+            this.btnlistele.TabIndex = 2;
+            this.btnlistele.Text = "Listele";
+            this.btnlistele.UseVisualStyleBackColor = true;
+            this.btnlistele.Click += new System.EventHandler(this.btnlistele_Click);
+            // 
+            // btnsil
+            // 
+            this.btnsil.Location = new System.Drawing.Point(375, 203);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(103, 42);
+            this.btnsil.TabIndex = 3;
+            this.btnsil.Text = "Sil";
+            this.btnsil.UseVisualStyleBackColor = true;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // btnara
+            // 
+            this.btnara.Location = new System.Drawing.Point(375, 261);
+            this.btnara.Name = "btnara";
+            this.btnara.Size = new System.Drawing.Size(103, 42);
+            this.btnara.TabIndex = 4;
+            this.btnara.Text = "Ara";
+            this.btnara.UseVisualStyleBackColor = true;
+            this.btnara.Click += new System.EventHandler(this.btnara_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 90);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(345, 282);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // txtbxkitapara
+            // 
+            this.txtbxkitapara.Location = new System.Drawing.Point(90, 41);
+            this.txtbxkitapara.Name = "txtbxkitapara";
+            this.txtbxkitapara.Size = new System.Drawing.Size(159, 22);
+            this.txtbxkitapara.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(490, 402);
+            this.Controls.Add(this.txtbxkitapara);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnara);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.btnlistele);
+            this.Controls.Add(this.btnkaydet);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnkaydet;
+        private System.Windows.Forms.Button btnlistele;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnara;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtbxkitapara;
+    }
+}
+
---
+++ kütüphane/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kütüphane
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-4PM5V54\SQLEXPRESS;Initial Catalog=KITAPLIK;Integrated Security=True;");
+
+        private void btnlistele_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select * from kıtaplık", baglanti);
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into kıtaplık (kitap_adi) values(@p1)", baglanti);
+            komut.Parameters.AddWithValue("@p1", txtbxkitapara.Text);
+            komut.ExecuteNonQuery();
+            MessageBox.Show("Kayıt eklendi.");
+            baglanti.Close();
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("delete from kıtaplık where kitap_adi=@p1", baglanti);
+            komut2.Parameters.AddWithValue("@p1", txtbxkitapara.Text);
+            komut2.ExecuteNonQuery();
+            MessageBox.Show("Silme başarılı.");
+            baglanti.Close();
+        }
+
+        private void btnara_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("select * from kıtaplık where kitap_adi=@p1", baglanti);
+            komut2.Parameters.AddWithValue("@p1", txtbxkitapara.Text);
+            SqlDataAdapter da= new SqlDataAdapter(komut2);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+    }
+
+    
+}
